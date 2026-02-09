17a257912f39c6c4d2c3cac33c843b376d4622a3
ZCodingSpace <104849364+ZCodingSpace@users.noreply.github.com>
2026-02-09 22:58:23 +0300

    Add TEST examples

--- JavaScript/20260101 Ch01 أرباح بائع الأضاحي.js
+++ JavaScript/20260101 Ch01 أرباح بائع الأضاحي.js
@@ -1,11 +1,47 @@
 
 function calculateSacrificeProfit(buyPrices, sellPrices) {
 
-    let sum = 0;
+    let totalProfit = 0;
+
     for (let i = 0; i < buyPrices.length; i++) {
-
-        sum += (sellPrices[i] - buyPrices[i])
+        totalProfit += sellPrices[i] - buyPrices[i];
     }
-    return sum;
 
+    return totalProfit;
 }
+
+
+///// TEST 1 /////
+// Inputs
+buyPrices = [100]
+sellPrices = [150]
+
+// Outputs
+console.log(calculateSacrificeProfit(buyPrices, sellPrices)) // 50
+
+
+///// TEST 2 /////
+// Inputs
+buyPrices = [100, 200]
+sellPrices = [150, 250]
+
+// Outputs
+console.log(calculateSacrificeProfit(buyPrices, sellPrices)) // 100
+
+
+///// TEST 3 /////
+// Inputs
+buyPrices = [120, 80, 100]
+sellPrices = [130, 90, 110]
+
+// Outputs
+console.log(calculateSacrificeProfit(buyPrices, sellPrices)) // 30
+
+
+///// TEST 4 /////
+// Inputs
+buyPrices = [200, 200, 200]
+sellPrices = [200, 200, 200]
+
+// Outputs
+console.log(calculateSacrificeProfit(buyPrices, sellPrices)) // 0
