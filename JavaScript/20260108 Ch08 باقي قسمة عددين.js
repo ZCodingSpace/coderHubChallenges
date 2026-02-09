089f42ba94fbf7a0a4257b9b8e4f7d98bf05c2f6
ZCodingSpace <104849364+ZCodingSpace@users.noreply.github.com>
2026-02-09 19:49:30 +0000

    20260108 Ch08 - Added for the first time

---
+++ JavaScript/20260108 Ch08 باقي قسمة عددين.js
@@ -0,0 +1,41 @@
+
+function div_remainder(a, b) {
+
+    return (a % b);
+}
+
+
+///// TEST 1 /////
+// Inputs
+a = 3
+b = 3
+
+// Outputs
+console.log(div_remainder(a, b)); // 0
+
+
+///// TEST 2 /////
+// Inputs
+a = 19
+b = 5
+
+// Outputs
+console.log(div_remainder(a, b)); // 4
+
+
+///// TEST 3 /////
+// Inputs
+a = 77
+b = 5
+
+// Outputs
+console.log(div_remainder(a, b)); // 2
+
+
+///// TEST 4 /////
+// Inputs
+a = 99
+b = 4
+
+// Outputs
+console.log(div_remainder(a, b)); // 3
