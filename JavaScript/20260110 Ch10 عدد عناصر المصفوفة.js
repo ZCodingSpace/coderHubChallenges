f8df5fd6f8871c6eb1ef70be1beb20251ca3975a
ZCodingSpace <104849364+ZCodingSpace@users.noreply.github.com>
2026-02-11 21:44:41 +0000

    20260110 Ch10 - Added for the first time

---
+++ JavaScript/20260110 Ch10 عدد عناصر المصفوفة.js
@@ -0,0 +1,35 @@
+
+function num_elements(my_array) {
+    return my_array.length;
+}
+
+///// TEST 1 /////
+// Inputs
+my_array = [5, 12, 3, 51]
+
+// Outputs
+console.log(num_elements(my_array)); // 4
+
+
+///// TEST 2 /////
+// Inputs
+my_array = [19, 12]
+
+// Outputs
+console.log(num_elements(my_array)); // 2
+
+
+///// TEST 3 /////
+// Inputs
+my_array = [10, 24, 45, 123, 124, 982, 51, 42, 124, 19]
+
+// Outputs
+console.log(num_elements(my_array)); // 10
+
+
+///// TEST 4 /////
+// Inputs
+my_array = [10]
+
+// Outputs
+console.log(num_elements(my_array)); // 1
