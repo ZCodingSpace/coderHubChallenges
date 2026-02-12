7cf0e9ebbcaecba2f70f49b6064b4901ba85041c
ZCodingSpace <104849364+ZCodingSpace@users.noreply.github.com>
2026-02-12 19:46:50 +0000

    Edit comments

--- JavaScript/20260103 Ch03 مساحة المعين.js
+++ JavaScript/20260103 Ch03 مساحة المعين.js
@@ -3,32 +3,38 @@ function area_of_rhombus(q, p) {
     return (q * p) / 2;
 }
 
-// --------------------
-// Test 1
-//     Inputs
-//         q = 6
-//         p = 7
-//     outputs
-//         21
-// --------------------
-// Test 2
-//     Inputs
-//         q = 3
-//         p = 6.6
-//     outputs
-//         9.9
-// --------------------
-// Test 3
-//     Inputs
-//         q = 8.7
-//         p = 9.1
-//     outputs
-//         39.58
-// --------------------
-// Test 4
-//     Inputs
-//         q = 9.2
-//         p = 8.6
-//     outputs
-//         39.56
-// --------------------
+
+///// TEST 1 /////
+// Inputs
+q = 6
+p = 7
+
+// Outputs
+console.log(area_of_rhombus(q, p)); // 21
+
+
+///// TEST 2 /////
+// Inputs
+q = 3
+p = 6.6
+
+// Outputs
+console.log(area_of_rhombus(q, p)); // 9.9
+
+
+///// TEST 3 /////
+// Inputs
+q = 8.7
+p = 9.1
+
+// Outputs
+console.log(area_of_rhombus(q, p)); // 39.58
+
+
+///// TEST 4 /////
+// Inputs
+q = 9.2
+p = 8.6
+
+// Outputs
+console.log(area_of_rhombus(q, p)); // 39.56
