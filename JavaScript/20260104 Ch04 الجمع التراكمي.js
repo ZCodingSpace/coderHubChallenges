2ee5c9e42ebc3bd28d345f09d527488e4f8985f6
ZCodingSpace <104849364+ZCodingSpace@users.noreply.github.com>
2026-02-15 18:03:22 +0000

    Edit comments

--- JavaScript/20260104 Ch04 الجمع التراكمي.js
+++ JavaScript/20260104 Ch04 الجمع التراكمي.js
@@ -1,42 +1,43 @@
 
 function cumulative_addition(elements_array) {
 
-    let result = []
-    let count = 0;
-    for (let i of elements_array) {
-        count += i;
-    }
-    result[0] = count;
-    result[1] = elements_array.length;
+    let sum = 0;
 
-    return result
+    for (let i of elements_array) {
+        sum += i;
+    }
+    // return the sum and the length of the array as an array
+    return [sum, elements_array.length];
 }
 
-elements_array = [5, 1, 2, 4, 9, 10, 200];
-console.log(cumulative_addition(elements_array));
 
-// --------------------
-// Test 1
-//     Inputs
-//         elements_array = [5, 1, 2, 4, 9, 10, 200]
-//     outputs
-//         [231,7]
-// --------------------
-// Test 2
-//     Inputs
-//         elements_array = [500, 123, 52, 123, 22]
-//     outputs
-//         [820,5]
-// --------------------
-// Test 3
-//     Inputs
-//         elements_array = [1, 1, 2, 5, 2, 5, 8]
-//     outputs
-//         [24,7]
-// --------------------
-// Test 4
-//     Inputs
-//         elements_array = [7, 9, 12, 100, 244]
-//     outputs
-//         [372,5]
-// --------------------
+///// TEST 1 /////
+// Inputs
+elements_array = [5, 1, 2, 4, 9, 10, 200]
+
+// Outputs
+console.log(cumulative_addition(elements_array)) // [231,7]
+
+
+///// TEST 2 /////
+// Inputs
+elements_array = [500, 123, 52, 123, 22]
+
+// Outputs
+console.log(cumulative_addition(elements_array)) // [820, 5]
+
+
+///// TEST 3 /////
+// Inputs
+elements_array = [1, 1, 2, 5, 2, 5, 8]
+
+// Outputs
+console.log(cumulative_addition(elements_array)) // [24, 7]
+
+
+///// TEST 4 /////
+// Inputs
+elements_array = [7, 9, 12, 100, 244]
+
+// Outputs
+console.log(cumulative_addition(elements_array)) // [372, 5]
