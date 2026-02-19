17c4f612f9779c295120891a703916dcebda1c4b
ZCodingSpace <104849364+ZCodingSpace@users.noreply.github.com>
2026-02-19 01:00:29 +0000

    20260124 Ch24 - Added for the first time

---
+++ JavaScript/20260124 Ch24 محيط المعين.js
@@ -0,0 +1,36 @@
+
+function circumference_of_rhombus(length) {
+    return 4 * length;
+}
+
+
+///// TEST 1 /////
+// Inputs
+length = 12
+
+// Outputs
+console.log(circumference_of_rhombus(length)); // 48
+
+
+///// TEST 2 /////
+// Inputs
+length = 7
+
+// Outputs
+console.log(circumference_of_rhombus(length)); // 28
+
+
+///// TEST 3 /////
+// Inputs
+length = 2
+
+// Outputs
+console.log(circumference_of_rhombus(length)); // 8
+
+
+///// TEST 4 /////
+// Inputs
+length = 3.3
+
+// Outputs
+console.log(circumference_of_rhombus(length)); // 13.2
