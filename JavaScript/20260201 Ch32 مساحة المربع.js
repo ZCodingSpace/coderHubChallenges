0f9169c95f6525d09c269adffea8b272a3fa000d
ZCodingSpace <104849364+ZCodingSpace@users.noreply.github.com>
2026-02-21 00:32:06 +0000

    20260201 Ch32 - Added for the first time

---
+++ JavaScript/20260201 Ch32 مساحة المربع.js
@@ -0,0 +1,36 @@
+
+function AreaOfSquare(length) {
+    return length * length;
+}
+
+
+///// TEST 1 /////
+// Inputs
+length = 3
+
+// Outputs
+console.log(AreaOfSquare(length)); // 9
+
+
+///// TEST 2 /////
+// Inputs
+length = 8
+
+// Outputs
+console.log(AreaOfSquare(length)); // 64
+
+
+///// TEST 3 /////
+// Inputs
+length = 5
+
+// Outputs
+console.log(AreaOfSquare(length)); // 25
+
+
+///// TEST 4 /////
+// Inputs
+length = 2
+
+// Outputs
+console.log(AreaOfSquare(length)); // 4
