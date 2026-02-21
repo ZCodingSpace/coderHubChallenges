488e8faa9fe17f1fe93e009c608e6c118e62e4da
ZCodingSpace <104849364+ZCodingSpace@users.noreply.github.com>
2026-02-21 00:27:12 +0000

    20260131 Ch31 - Added for the first time

---
+++ JavaScript/20260131 Ch31 مساحة متوازي الأضلاع.js
@@ -0,0 +1,40 @@
+
+function areaOfParallelogram(height, base_value) {
+    return height * base_value;
+}
+
+
+///// TEST 1 /////
+// Inputs
+height = 3
+base_value = 8
+
+// Outputs
+console.log(areaOfParallelogram(height, base_value)); // 24
+
+
+///// TEST 2 /////
+// Inputs
+height = 1
+base_value = 4
+
+// Outputs
+console.log(areaOfParallelogram(height, base_value)); // 4
+
+
+///// TEST 3 /////
+// Inputs
+height = 2.3
+base_value = 9
+
+// Outputs
+console.log(areaOfParallelogram(height, base_value)); // 20.7
+
+
+///// TEST 4 /////
+// Inputs
+height = 4.3
+base_value = 3.2
+
+// Outputs
+console.log(areaOfParallelogram(height, base_value)); // 13.76
